8cc6653f4efa88efee2fc808c6d53f1f2207c7fb
Horácio Dias <hneto1@uolinc.com>
2026-05-27 09:40:59 -0300

    removendo node e npm dependencies

--- app/static/src/store.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import modules from './store/modules/index'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  modules: modules
-})
--- app/static/src/store/modules/login/actions.js
+++
@@ -1,3 +0,0 @@
-export const LOGIN_DO_LOGIN = 'login.LOGIN_DO_LOGIN'
-export const LOGIN_CLEAR_MESSAGE = 'login.LOGIN_CLEAR_MESSAGE'
-export const LOGIN_VERIFY_COOKIE = 'login.LOGIN_VERIFY_COOKIE'
